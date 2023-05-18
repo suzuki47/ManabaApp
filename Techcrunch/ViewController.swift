ab6ee799d67c2b4bad9643352c0346bd52bad354
suzuki <is0620fh@ed.ritsumei.ac.jp>
2023-05-18 11:47:05 +0900

    Initial Commit

---
+++ Techcrunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techcrunch
+//
+//  Created by 鈴木悠太 on 2023/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
